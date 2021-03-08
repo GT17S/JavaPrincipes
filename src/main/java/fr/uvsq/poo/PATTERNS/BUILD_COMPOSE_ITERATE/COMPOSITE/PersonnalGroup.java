580983ecb59a7314c56e31280a75f374fe09422e
GT17S <guenanetoufik0615@gmail.com>
2021-03-08 15:58:44 +0100

    Création de la classe PersonnalGrou^p.java pour le pattern de composite

---
+++ src/main/java/fr/uvsq/poo/PATTERNS/BUILD_COMPOSE_ITERATE/COMPOSITE/PersonnalGroup.java
@@ -0,0 +1,17 @@
+package fr.uvsq.poo.PATTERNS.BUILD_COMPOSE_ITERATE.COMPOSITE;
+
+/**
+ * La classe <code>PersonnalGroup</code> représente la classe de d'un groupe de Personnel de travail.
+ *
+ * @author GT17S GUENANE Toufik (uvsq21807955)
+ * @version 2021
+ */
+
+public class PersonnalGroup extends PersonnalType {
+
+    
+    @Override
+    public boolean isGroup() {
+        return true;
+    }
+}
